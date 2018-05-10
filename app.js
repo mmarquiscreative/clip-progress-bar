0b15db57fae05ce38670cb45fe8c3967bcdde816
mmarquiscreative <mmarquis.creative@gmail.com>
2018-05-09 17:05:10 -0700

    asdfsdaf

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ var formApp = angular.module('formApp', [])
     
    
  var rotation = percentToDegree(chart.score);
-       var fix_rotation = rotation *2;
+       var fix_rotation = rotation * 2;
  console.log(rotation);
-  document.querySelector('.circle .fill, .circle .half').style.transform = ('rotate(' + rotation + 'deg)');
+  document.querySelector('.circle .fill, .circle .mask.full, .circle .mask.half').style.transform = ('rotate(' + rotation + 'deg)');
        
-       document.querySelector('.circle .fill.fix, .circle .full').style.transform = ('rotate(' + fix_rotation + 'deg)');
+       document.querySelector('.circle .fill.fix').style.transform = ('rotate(' + fix_rotation + 'deg)');
     };
     function percentToDegree(percentNum){
         var returnDegree = (percentNum/100) * 180;
